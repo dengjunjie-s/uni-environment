1c71fa908caa9f828d7b2b92bd7f8b499d899695
dengjunjie <986387834@qq.com>
2023-05-04 10:01:16 +0800

    备份架构

---
+++ .eslintrc.cjs
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-essential',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended' //必须放在最后面
+  ],
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module'
+  },
+  ignorePatterns: ['dist'],
+  plugins: ['vue', '@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/no-explicit-any': ['off'],
+    'vue/multi-word-component-names': 0,
+    '@typescript-eslint/ban-ts-comment': 0,
+    'prefer-const': 1,
+    semi: [1, 'always'],
+    'no-undef': 0,
+    'no-empty': 0,
+    '@typescript-eslint/no-empty-interface': 0
+  }
+};
---
+++ src/pages/index/index.vue
@@ -0,0 +1,63 @@
+<template>
+  <view class="content">
+    <view class="text-area">
+      <text class="title">{{ title }}</text>
+    </view>
+  </view>
+  <view style="padding: 20px">
+    <u-button type="primary" text="确定"></u-button>
+    <u-button type="primary" :plain="true" text="镂空"></u-button>
+    <u-button
+      type="primary"
+      :plain="true"
+      :hairline="true"
+      text="细边"
+    ></u-button>
+    <u-button type="primary" text="禁用"></u-button>
+    <u-button type="primary" loading loadingText="sadad">
+      wwsadasdsadaw
+    </u-button>
+    <u-button type="primary" icon="map" text="图标按钮"></u-button>
+    <u-button type="primary" shape="circle" text="按钮形状"></u-button>
+    <u-button
+      text="渐变色按钮"
+      color="linear-gradient(to right, rgb(66, 83, 216), rgb(213, 51, 186))"
+      >ss</u-button
+    >
+    <u-button type="primary" size="small" text="大小尺寸"></u-button>
+  </view>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+const data = '';
+const title = ref('Hello');
+</script>
+
+<style lang="scss">
+.content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.logo {
+  height: 200rpx;
+  width: 200rpx;
+  margin-top: 200rpx;
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 50rpx;
+}
+
+.text-area {
+  display: flex;
+  justify-content: center;
+}
+
+.title {
+  font-size: 36rpx;
+  color: #8f8f94;
+}
+</style>
